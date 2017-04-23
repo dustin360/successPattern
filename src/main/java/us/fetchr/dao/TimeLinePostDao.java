e8847cca65c234b83ad3069f6a8981c1008aa7ef
David <davidoyebanji2000@gmail.com>
2017-04-23 01:28:12 +0100

    my jar

--- src/main/java/us/fetchr/dao/TimeLinePostDao.java
+++
@@ -1,9 +0,0 @@
-package us.fetchr.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import us.fetchr.model.TimeLinePost;
-
-public interface TimeLinePostDao extends JpaRepository<TimeLinePost, Long>{
-	TimeLinePost findByTimelinePost(String timelinePost);
-}
